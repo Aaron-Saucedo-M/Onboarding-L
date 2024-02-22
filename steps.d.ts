7e60c899109e87d2899fbb6c6e52bf2ed78f8b83
Aaron-Saucedo-M <aaron.saucedo@wizeline.com>
2024-02-22 13:23:27 -0600

    Task 2 - First Commit

--- steps.d.ts
+++ steps.d.ts
@@ -1,11 +1,16 @@
 /// <reference types='codeceptjs' />
 type steps_file = typeof import('./steps_file.js');
+type janusObit = typeof import('./pages/JanusObit.PO');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any }
-  interface Methods extends WebDriver {}
-  interface I extends ReturnType<steps_file> {}
-  namespace Translation {
-    interface Actions {}
-  }
+    interface SupportObject {
+        I: I;
+        current: any;
+        janusObitPage: janusObit;
+    }
+    interface Methods extends WebDriver {}
+    interface I extends ReturnType<steps_file> {}
+    namespace Translation {
+        interface Actions {}
+    }
 }
--- test/Task1_test.spec.js
+++ test/Task1_test.spec.js
@@ -8,31 +8,13 @@ Feature('Task 1');
  * - Submit a Guest book entry
  * - Validate entry is submitted successfully
  */
-Scenario('Task 1 - Leave Guest Book Entry', ({ I }) => {
+Scenario('Task 1 - Leave Guest Book Entry', ({ I, janusObitPage }) => {
     // Load page
     I.amOnPage(process.env.URL);
-    
+
     // Validate first name
-    I.see('Virginia', { css: 'h2[data-component="NameHeadingText"]' });
+    I.see('Virginia', janusObitPage.headerPersonName);
 
-    // Submit Guest Book Entry
-    I.scrollIntoView({ css: 'textarea[name="_GuestbookForm_Message"]' });
-    I.fillField(
-        { css: 'textarea[name="_GuestbookForm_Message"]' },
-        'Test Message'
-    );
-    I.fillField({ css: 'input[name="_GuestbookForm_From"]' }, 'Test From');
-    I.fillField(
-        { css: 'input[name="_GuestbookForm_Email"]' },
-        'Test@email.com'
-    );
-    I.click({
-        css: 'button[data-component = "AffiliateGuestbookSubmitButton"]',
-    });
-
-    // Wait for entry to be submitted
-    I.waitForElement(
-        { css: 'div[data-component="AffiliateGuestbookSuccessBox"]' },
-        10
-    );
+    // Fill and submit guestbook form
+    janusObitPage.createGuestBookEntry();
 });
