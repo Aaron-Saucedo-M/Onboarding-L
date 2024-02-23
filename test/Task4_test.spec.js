eff52389fd8eb74fd1e811f61533c428569064d5
Aaron-Saucedo-M <aaron.saucedo@wizeline.com>
2024-02-23 12:07:04 -0600

    Task 4 - First Commit

---
+++ test/Task4_test.spec.js
@@ -0,0 +1,16 @@
+Feature('Task 4 - API');
+
+/**
+ * @Author Aaron
+ * @Steps
+ * - Try to access the page via API with valid pid and invalid pid
+ * - Validate responses
+ */
+const api = new DataTable(['url', 'statusCode']);
+api.add(['/virginia-gruchalski-obituary?pid=196167379', 200]);
+api.add(['/virginia-gruchalski-obituary?pid=0', 404]);
+
+Data(api).Scenario('Test API', async ({ I, current }) => {
+    const response = await I.sendGetRequest(current.url);
+    I.assert(response.status, current.statusCode);
+});
